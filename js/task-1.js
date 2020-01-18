331a44586276dcc36c7cdffcd373341011200441
Olena Savchenko <olsavchenko1@gmail.com>
2020-01-18 21:50:34 +0200

    reworked tasks 1 and 4 by mentor's comments

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,6 @@
-// const priceOfGoods = 1000;
-const priceOfGoods = 2000;
+let priceOfGoods = 1000;
 
 const nameOfGoods = 'Генератор защитного поля';
+console.log(`Выбран ${nameOfGoods}, цена за штуку ${priceOfGoods} кредитов.`);
+priceOfGoods = 2000;
 console.log(`Выбран ${nameOfGoods}, цена за штуку ${priceOfGoods} кредитов.`);
--- js/task-4.js
+++ js/task-4.js
@@ -3,15 +3,17 @@ const pricePerDroid = 3000;
 let message;
 let userIhput = prompt('Пожалуйста укажите количество приобретаемых дроидов');
 userIhput = Number(userIhput);
-const totalPrice = userIhput * pricePerDroid;
 
 if (userIhput === null) {
   message = 'Отменено пользователем!';
-} else if (totalPrice > credits) {
-  message = 'Недостаточно средств на счету!';
 } else {
-  message = `Вы купили ${userIhput} дроидов, на счету осталось ${credits -
-    totalPrice} кредитов.`;
+  const totalPrice = userIhput * pricePerDroid;
+
+  message =
+    totalPrice > credits
+      ? 'Недостаточно средств на счету!'
+      : `Вы купили ${userIhput} дроидов, на счету осталось ${credits -
+          totalPrice} кредитов.`;
 }
 
 console.log(message);
